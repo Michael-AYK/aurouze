8f11287ddad9a5e235f9164a661b5b1de9873195
Michael-AYK <71391713+Michael-AYK@users.noreply.github.com>
2021-09-10 17:24:29 +0100

    Add files via upload

---
+++ src/helpers/b_items_data.js
@@ -0,0 +1,55 @@
+import b1 from '../assets/b1.jpg'
+import b2 from '../assets/b2.jpg'
+import b3 from '../assets/b3.jpg'
+import b4 from '../assets/b4.jpg'
+import b5 from '../assets/b5.jpg'
+import b6 from '../assets/b6.png'
+import b7 from '../assets/b7.jpg'
+import b8 from '../assets/b8.jpg'
+
+export const b_data_1 = [
+    {
+        imgSrc: b1,
+        name: 'Tapette à Rat Bois',
+        price: '2,50 €'
+    },
+    {
+        imgSrc: b2,
+        name: 'Répulsif Anti Fouine et Martre',
+        price: '13.00 €'
+    },
+    {
+        imgSrc: b3,
+        name: 'Piège à glu Rat et Souris',
+        price: '9,50 €'
+    },
+    {
+        imgSrc: b4,
+        name: 'Répulsif Anti Taupe Solaire',
+        price: '26,00 €'
+    }
+    
+]
+
+export const b_data_2 = [
+    {
+        imgSrc: b5,
+        name: 'Tapette à Rat fer',
+        price: '3,50 €'
+    },
+    {
+        imgSrc: b6,
+        name: 'Anti Souris Mouse Stop',
+        price: '19,50 €'
+    },
+    {
+        imgSrc: b7,
+        name: 'Ultrasons Pile et Secteur',
+        price: '27,00 €'
+    },
+    {
+        imgSrc: b8,
+        name: 'Piège à Ragondin',
+        price: '110,00 €'
+    }
+]
---
+++ src/helpers/h_items_data.js
@@ -0,0 +1,27 @@
+import h1 from '../assets/h1.png'
+import h2 from '../assets/h2.png'
+import h3 from '../assets/h3.png'
+import h4 from '../assets/h4.png'
+
+export const data = [
+    {
+        imgSrc: h1,
+        title: 'DÉRATISATION',
+        content: 'Débarrassez-vous une bonne fois pour toute des souris, mulots, rats, loirs, fouines, taupes, ragondins, campagnols...'
+    },
+    {
+        imgSrc: h2,
+        title: 'DÉSINSECTISATION',
+        content: 'Débarrassez-vous une bonne fois pour toute des souris, mulots, rats, loirs, fouines, taupes, ragondins, campagnols...'
+    },
+    {
+        imgSrc: h3,
+        title: 'DÉPIGEONNISATION',
+        content: 'Débarassez-vous des pigeons, autres volatiles...'
+    },
+    {
+        imgSrc: h4,
+        title: 'DÉSINSECTION',
+        content: 'Avec nos produits, dites au revoir odeurs acariens, virus, rubérole, champignons, mycos, protozoaires..'
+    }
+]
