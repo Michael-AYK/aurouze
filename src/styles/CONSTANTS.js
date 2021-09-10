8dd5b1903144569109696912f63b985c836722cf
Michael-AYK <71391713+Michael-AYK@users.noreply.github.com>
2021-09-10 17:31:17 +0100

    Add files via upload

---
+++ src/styles/CONSTANTS.js
@@ -0,0 +1,14 @@
+export default {
+    palette: {
+        primary: {
+            main: "669128",
+            darken: "275101"
+        },
+        secondary: {
+
+        },
+        others: {
+            white: '#fff'
+        }
+    }
+}
